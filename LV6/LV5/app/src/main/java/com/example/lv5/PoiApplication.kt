696f0281a5034e64c8c20f77e745fc12dc24c6ea
Mihajlo Sofronijević <mihajlo.sofronijevic@elfak.rs>
2024-05-31 16:25:28 +0200

    Brisanje LV6

--- LV6/LV5/app/src/main/java/com/example/lv5/PoiApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.example.lv5
-
-import android.app.Application
-import com.google.firebase.Firebase
-import com.google.firebase.firestore.firestore
-
-class PoiApplication: Application() {
-    val db by lazy {Firebase.firestore}
-}
